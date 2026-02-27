0f48123f84cf4e4d6126f0f55a7d99c7724bd0f5
sid <bhosalesid777@gmail.com>
2026-02-27 16:29:29 +0530

    initialized repo

---
+++ Demo.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Demo {
+    
+   public static void main ( String [] args ){
+       Scanner input = new Scanner(System.in);
+       int a = input.nextInt();
+       
+       System.out.println(a + "thsis is the integer ");
+       input.close();
+   }
+}
