44031d5659d43cf67de5c728f702a3032b28220a
sid <bhosalesid777@gmail.com>
2026-03-06 16:47:03 +0530

    new input file added

---
+++ TakingInputWithBufferedReader.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+class TakingInputWithBufferedReader{
+    public static void main ( String []args) throws IOException 
+    {
+        
+        // taking input in string  with the buffered reader and then printing it 
+        System.out.println("hello there");
+        System.out.println("input your name");
+        BufferedReader br = new BufferedReader ( new InputStreamReader(System.in));
+        String name = br.readLine();
+        System.out.println(name);
+        
+        // taking input in the int format with buffered reader  and then printing it 
+        // 
+        System.out.println("enter you age and press enter");
+        int age =Integer.parseInt (br.readLine());
+        System.out.println(age);
+    }
+}
